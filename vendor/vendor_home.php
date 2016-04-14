efe34e8eb47590bb2b054359049e93c44e7cea69
Mukesh A <amukesh.mk@gmail.com>
2016-04-15 00:39:31 +0530

    dynamic item listing - vendor
    dynamic item listing - vendor page

--- vendor/vendor_home.php
+++ vendor/vendor_home.php
@@ -1,10 +1,170 @@
 <?php
 	require_once('../login/auth.php');
 ?>
-<html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Pay Now - Customer</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="../assets/css/form-elements.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
+    
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="../assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    </head>
     <body>
-        <h1>
-            Vendor Home !! XD
-        </h1>
-    </body>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="#">PayNow</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="vendor_home.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="vendor_cart.php">Cart</a>
+                    </li>
+                    <li>
+                        <a href="vendor_sett.php">Settings</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+        			<li><a href='../index.php'><span class="glyphicon glyphicon-off"></span> Log Out</a></li>
+      			</ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>		
+    </body> 
+    
+    
+    
+    <?php
+	echo'<br><br>';
+	$connect=mysqli_connect("localhost","root","");
+    if (!$connect) 
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    else
+	{
+        mysqli_set_charset($connect, 'utf8');
+        if (!mysqli_select_db($connect, 'paynow')) 
+        { 
+            echo "Unable to locate database."; 
+        }
+    }
+    $result = mysqli_query($connect, "SELECT item_no, item_nm, item_price, v_id FROM item_list where v_id = '" .$_SESSION['SESS_MEMBER_ID']."'");
+    if (!$result)  
+    {  
+    	echo "Error fetching data: " . mysqli_error($connect);  
+    }
+	
+	if(mysqli_num_rows($result) > 0) 
+    {
+		while ($row = mysqli_fetch_array($result))
+    	{
+    		$item_no[] = $row['item_no'];
+    		$item_nm[] = $row['item_nm'];  
+    		$item_price[] = $row['item_price'];
+    		$v_id[] = $row['v_id'];
+    	}
+	
+    ?>
+  
+    
+    
+    
+    	<div class="container">		
+			<div class="container">
+	          <div class="row">
+               	<div class="row">
+                <?php 
+						$x=0; 
+						foreach ($item_no as $i_no):
+                    		echo "
+                <div class=\"col-md-3 text-center\" >
+                    <div class=\"box\"\">
+                        <div class=\"box-content\">";        
+                        echo '
+                            <div class=" hero-feature">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/800x500" alt="">
+                                    <div class="caption">
+                                        <h3><strong>Item Name: </strong>'.$item_nm[$x].'</h3>
+                                        <p><strong>Price :</strong>'.$item_price[$x].'</p>
+                                        <p>
+                                        <a href="" class="btn btn-primary" onclick="myFunction()">Add to Cart</a> 
+                                        <a href="#" class="btn btn-default">More Info</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+                        $x=$x+1; 
+                        endforeach; 
+	}
+	else
+	{
+		echo'<h1 align="center">NO Notes to Display !!</h1>';
+	}
+				?>
+                    
+                </div>           
+            </div>
+	   </div>
+    </div>
+    <!-- /.container -->
+
+        <script src="../assets/js/jquery-1.11.1.min.js"></script>
+        <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../assets/js/jquery.backstretch.min.js"></script>
+        <script src="../assets/js/scripts.js"></script>
+<!--    </body>-->
+    <style>
+        .nav-tabs
+        {
+            background-color:#fff;
+        }
+        .tab-content
+        {
+            background-color:#fff;
+            color:#fff;
+            padding:5px
+        }
+        .nav-tabs > li > a
+        {
+            border: medium none;
+        }
+        .nav-tabs > li > a:hover
+        {
+            background-color: #fff !important;
+            border: medium none;
+            border-radius: 0;
+            color:#161616;
+        }
+    </style>
+
 </html>
